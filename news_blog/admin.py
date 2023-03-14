5343ae1d1691abd79b8fdb384b844dddac4a6f1e
qoterwoter <marderics@gmail.com>
2023-03-14 19:07:29 +0300

    news blog logic

--- news_blog/admin.py
+++ news_blog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.forms import ClearableFileInput
+from django.db import models
 
-# Register your models here.
+from .models import News, NewsArticle
+
+
+class NewsArticleInline(admin.TabularInline):
+    list_display = ('   ')
+    model = NewsArticle
+    extra = 1
+    formfield_overrides = {
+        models.ImageField: {'widget': ClearableFileInput(attrs={'multiple': True})},
+    }
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at', 'author', 'display_articles')
+
+    inlines = [NewsArticleInline]
+
+    def display_articles(self, obj):
+        return ', '.join([article.content for article in obj.articles.all()])
+
+    display_articles.short_description = 'Articles'
+    inlines = [NewsArticleInline]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['author'].initial = request.user.id
+        return form
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+
+# @admin.register(NewsArticle)
+# class NewsArticleAdmin(admin.ModelAdmin):
+#     list_display = ('news', 'image_tag')
+#     readonly_fields = ('image_tag',)
+#     formfield_overrides = {
+#         models.ImageField: {'widget': ClearableFileInput},
+#     }
+
+#     def image_tag(self, obj):
+#         if obj.image:
+#             return '<img src="%s" width="150px" />' % obj.image.url
+#         else:
+#             return '(no image)'
+#     image_tag.allow_tags = True
+#     image_tag.short_description = 'Image'
--- news_blog/models.py
+++ news_blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField('Заголовок', max_length=100)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'is_staff': True, 'is_superuser': True})
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class NewsArticle(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='articles', verbose_name='Новость')
+    content = models.TextField('Контент')
+    image = models.ImageField('Изображение', upload_to='news/', blank=True, null=True)
+    caption = models.CharField('Описание изображения', max_length=100, blank=True)
+
+    class Meta:
+        verbose_name = 'Абзац'
+        verbose_name_plural = 'Абзацы'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.caption or self.image.name or str(self.id)
