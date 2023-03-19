01470bf15490ef481d1806f341784028167f8545
qoterwoter <marderics@gmail.com>
2023-03-19 18:11:11 +0300

    allowed hosts

--- sonichaven/urls.py
+++ sonichaven/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from django.urls import include 
 from .views import RegistrationView, LoginView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('news_blog.urls')),
@@ -10,4 +13,5 @@ urlpatterns = [
     path('api/', include('staff_service.urls')),
     path('api/register/', RegistrationView.as_view()),
     path('api/login/', LoginView.as_view()),
+    static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 ]
