c16f6d8c89b494389a4212d9849af8b8ddb6af22
qoterwoter <marderics@gmail.com>
2023-03-20 17:17:25 +0300

    fix

--- sonichaven/urls.py
+++ sonichaven/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from django.urls import include 
 from .views import RegistrationView, LoginView
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('news_blog.urls')),
@@ -13,5 +10,4 @@ urlpatterns = [
     path('api/', include('staff_service.urls')),
     path('api/register/', RegistrationView.as_view()),
     path('api/login/', LoginView.as_view()),
-    static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 ]
