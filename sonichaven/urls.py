b7d37ed83e899f854ed639614e1e39e6a3ed8faf
qoterwoter <marderics@gmail.com>
2023-04-28 13:20:40 +0300

    urls

--- sonichaven/urls.py
+++ sonichaven/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include 
 from .views import RegistrationView, LoginView
+import corsheaders
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +12,5 @@ urlpatterns = [
     path('api/register/', RegistrationView.as_view()),
     path('api/login/', LoginView.as_view()),
 ]
+
+urlpatterns += [path('api/', include('corsheaders.urls'))]
