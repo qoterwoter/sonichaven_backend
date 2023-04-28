f9bb3fd7abca60e198f59099fc14d174510aee96
qoterwoter <marderics@gmail.com>
2023-04-28 13:17:36 +0300

    m

--- sonichaven/settings.py
+++ sonichaven/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000',]
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://sonichaven.std-962.ist.mospolytech.ru',
