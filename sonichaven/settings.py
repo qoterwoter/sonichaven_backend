fc23899048a61adea7479dbe5a42d141849e8c3c
qoterwoter <marderics@gmail.com>
2023-04-28 12:50:53 +0300

    fix

--- sonichaven/settings.py
+++ sonichaven/settings.py
@@ -38,6 +38,20 @@ REST_FRAMEWORK = {
     ]
 }
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://sonichaven.std-962.ist.mospolytech.ru',]
+
 INSTALLED_APPS = [
     'artist_card.apps.ArtistCardConfig',
     'staff_service.apps.StaffServiceConfig',
@@ -54,20 +68,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders'
 ]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-]
-
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://sonichaven.std-962.ist.mospolytech.ru',]
 
 ROOT_URLCONF = 'sonichaven.urls'
 
