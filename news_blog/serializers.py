37a479cdd6463d140835c25eee96e5c113c3212e
qoterwoter <marderics@gmail.com>
2023-05-26 01:57:09 +0300

    asd

--- news_blog/serializers.py
+++ news_blog/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import News, NewsArticle
 
+
 class NewsArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsArticle
         fields = ('id', 'content', 'image', 'caption')
+
 
 class NewsSerializer(serializers.ModelSerializer):
     article = serializers.SerializerMethodField()
@@ -14,15 +16,16 @@ class NewsSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'created_at', 'updated_at', 'author', 'article')
 
     def get_article(self, obj):
-        article = obj.articles.first() # assuming you want to get the first article
+        article = obj.articles.first()  # assuming you want to get the first article
         if article:
             return NewsArticleSerializer(article).data
         else:
             return None
 
+
 class NewsDetailSerializer(serializers.ModelSerializer):
     articles = NewsArticleSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = News
         fields = ('id', 'title', 'created_at', 'updated_at', 'author', 'articles')
